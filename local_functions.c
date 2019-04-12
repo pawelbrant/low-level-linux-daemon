1c1f2b6ffec1d9eb08e881bbc59d0ebe4ee38b7c
Paweł Bąk <pawel.bak98@gmail.com>
2019-04-12 12:16:11 +0200

    New start

--- local_functions.c
+++ local_functions.c
@@ -1,60 +1,66 @@
 #include "local_functions.h"
 
-
 //checks whether main app has been called with valid parameters
-bool is_Call_Valid(int number, char *params[])
+int is_Call_Valid(int number_of_parameters, char *parameters[])
 {
-  if(number < 3)
+  if(number_of_parameters < 3)
   {
-    printf("Not enought input arguments\n");
     syslog(LOG_ERR, "Not enought input arguments");
-    return false;
+    return 0;
   }
-  if(is_Directory(params[1]))
+  if(get_Type(parameters[1]) == 0)
   {
-    if(is_Directory(params[2]))
-      return true;
-    else
+    if(get_Type(parameters[2]) == 0)
+    {
+      return 1;
+    }
     syslog(LOG_ERR, "Second argument is not a directory");
-    return false;
+    return 0;
   }
   syslog(LOG_ERR, "First argument is not a directory");
-  return false;
+  return 0;
 }
 
-//checks whether given path points a directory
-bool is_Directory(char *path)
+//checks whether given path points a directory or a file
+//file returns 1
+//directory returns 0
+//other return 2
+int get_Type(char *path)
 {
   struct stat s;
-  if(stat(path, &s) == 0)
+  if(lstat(path, &s) == 0)
   {
-    if(s.st_mode & S_IFDIR)
-      return true;
-    return false;
+    switch (s.st_mode & S_IFMT)
+    {
+      case S_IFDIR:  return 0;  break;
+      case S_IFREG:  return 1;  break;
+      default:       return 2;  break;
+    }
   }
   syslog(LOG_ERR, "Unable to get stat");
-  return false;
+  return -1;
 }
 
-//may be modified with following function to make one returning stat type
+//returns size of file
 off_t get_Size(char *input)
 {
   struct stat size;
   if(stat(input, &size) == -1)
   {
     syslog(LOG_ERR, "Can't get size of file %s", input);
-    exit(EXIT_FAILURE);
+    return -1;
   }
   return size.st_size;
 }
 
+//returns modification date of a file
 time_t get_Time(char *input)
 {
   struct stat time;
   if(stat(input, &time) == -1)
   {
     syslog(LOG_ERR, "Can't get modification data for %s", input);
-    exit(EXIT_FAILURE);
+    return -1;
   }
   return time.st_mtime;
 }
@@ -65,12 +71,12 @@ mode_t get_Permissions(char *input)
   if(stat(input, &permissions) == -1)
   {
     syslog(LOG_ERR, "Can't get permissions for %s", input);
-    exit(EXIT_FAILURE);
+    return -1;
   }
   return permissions.st_mode;
 }
 
-void change_Parameters(char *input, char *output)
+int change_Parameters(char *input, char *output)
 {
   struct utimbuf time;
   time.actime = 0;
@@ -78,62 +84,72 @@ void change_Parameters(char *input, char *output)
   if(utime(output, &time) != 0)
   {
     syslog(LOG_ERR, "Error with modification data.");
-    exit(EXIT_FAILURE);
+    return -1;
   }
+  return 0;
 }
 
 char *add_To_Path(char *path, char *file_name)
 {
   int len = strlen(path) + strlen(file_name) + 1;
   char *new_path = (char *)malloc(len * sizeof(char));
+  if(new_path == NULL)
+    return new_path;
   strcpy(new_path, path);
   strcat(new_path,"/");
   strcat(new_path,file_name);
   return new_path;
 }
 
-bool file_Comparing(char *file_name, char *input_folder_path, char *output_folder_path)
+int are_Same(char *file_name, char *input_folder_path, char *output_folder_path)
 {
-  char *old_path = add_To_Path(input_folder_path, file_name);
-  char *new_path = add_To_Path(output_folder_path, file_name);
-  bool check = 1;
+  char *old_path;
+  char *new_path;
+  bool check = 0;
   struct dirent *file;
   DIR *catalog_path;
-  catalog_path = opendir(output_folder_path);
+  if((catalog_path = opendir(output_folder_path)) == NULL)
+    return -1;
   while(file = readdir(catalog_path))
   {
     if(strcmp(file->d_name, file_name) == 0)
     {
-      if((file -> d_type) == DT_REG)
+      if((file -> d_type) == 8)
       {
+        old_path = add_To_Path(input_folder_path, file_name);
+        new_path = add_To_Path(output_folder_path, file_name);
         if(!(get_Time(old_path) - get_Time(new_path)))
-          check = 0;
+          check = 1;
       }
       else
       {
-        check = 0;
+        check = 1;
       }
     }
   }
-  // printf("In func file_Comparing: %s %s %s return value %d\n", file_name, input_folder_path, output_folder_path, check);
+  printf("In func are_Same: %s %s %s return value %d\n", file_name, input_folder_path, output_folder_path, check);
   closedir(catalog_path);
   return check;
 }
 
-void delete_File(char *input_folder_path, char *output_folder_path, bool recursive)
+int delete_File(char *input_folder_path, char *output_folder_path, bool recursive)
 {
   struct dirent *file, *content;
   DIR *catalog_path, *temp;
   char *old_path;
   char *new_path;
-  catalog_path = opendir(output_folder_path);
+  if((catalog_path = opendir(output_folder_path)) == NULL)
+  {
+    printf("%s\n", "Nie dziala");
+    return -1;
+  }
   while(file = readdir(catalog_path))
   {
     if((file -> d_type) == DT_DIR && recursive)
     {
       if(!(strcmp(file -> d_name,".") == 0 || strcmp(file -> d_name, "..") == 0))
       {
-        if(file_Comparing(file -> d_name, output_folder_path, input_folder_path))
+        if(are_Same(file -> d_name, output_folder_path, input_folder_path))
         {
           char *old_path = add_To_Path(input_folder_path, file -> d_name);
           char *new_path = add_To_Path(output_folder_path, file -> d_name);
@@ -161,7 +177,7 @@ void delete_File(char *input_folder_path, char *output_folder_path, bool recursi
     {
       char *old_path = add_To_Path(input_folder_path, file -> d_name);
       char *new_path = add_To_Path(output_folder_path, file -> d_name);
-      if(access(new_path, F_OK) == 0 && file_Comparing(file -> d_name, output_folder_path, input_folder_path))
+      if(access(new_path, F_OK) == 0 && are_Same(file -> d_name, output_folder_path, input_folder_path))
       {
         remove(new_path);
         syslog(LOG_INFO, "File %s deleted.", new_path);
@@ -229,14 +245,14 @@ void browse_Folder(char *input_folder_path, char *output_folder_path, bool recur
   char *file_name;
   char *old_path;
   char *new_path;
-  path = opendir(input_folder_path);
+  if((path = opendir(input_folder_path)) == NULL)
   while(file = readdir(path))
   {
     if((file -> d_type) == DT_REG)
     {
       old_path = add_To_Path(input_folder_path, file -> d_name);
       new_path = add_To_Path(output_folder_path, file -> d_name);
-      if(file_Comparing(file -> d_name, input_folder_path, output_folder_path))
+      if(are_Same(file -> d_name, input_folder_path, output_folder_path))
 
         if(get_Size(old_path)>size_of_file)
           copy_File_By_Mapping(old_path, new_path);
@@ -247,7 +263,7 @@ void browse_Folder(char *input_folder_path, char *output_folder_path, bool recur
     {
       if(!(strcmp(file -> d_name,".") == 0 || strcmp(file -> d_name,"..") == 0))
       {
-        file_Comparing(file -> d_name, input_folder_path, output_folder_path);
+        are_Same(file -> d_name, input_folder_path, output_folder_path);
         old_path = add_To_Path(input_folder_path, file -> d_name);
         new_path = add_To_Path(output_folder_path, file -> d_name);
         if(!(tmp = opendir(new_path)))
